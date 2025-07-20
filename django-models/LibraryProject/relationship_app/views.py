05f6ed936eae75af152df5dd865b0ef8ff23578c
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-20 21:30:49 +0300

    Add views and templates for listing books and displaying library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+from django.http import HttpRequest
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # The library object is automatically available as 'library' in the template
+        # The books can be accessed via library.books.all in the template
+        return context
