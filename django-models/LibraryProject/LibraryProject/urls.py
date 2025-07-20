d8991728f4984d48c3e3ee7e0e88243a8f041fc7
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-20 21:37:16 +0300

    Fix URL patterns in urls.py for consistency and clarity

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from relationship_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',views.book_list)
+    path('books/', views.book_list, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
