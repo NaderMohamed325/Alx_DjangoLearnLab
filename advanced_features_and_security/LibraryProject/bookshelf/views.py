00f63a78a03acb96e9ded9effd3dd0f4da883d62
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-26 16:26:22 +0300

    Import ExampleForm in views.py for future use

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -36,3 +36,7 @@ def delete_book(request, pk):
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+
+from .forms import ExampleForm
