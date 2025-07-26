82f7e8bdece0af92e911da0f04c5e7003635152d
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-26 13:14:16 +0300

    Rename CustomAdmin to CustomUserAdmin for consistency in user management

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,7 +7,7 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
 
-class CustomAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "date_of_birth", "is_staff")
     search_fields = ("username", "email")
     list_filter = ("is_staff", "is_superuser")
@@ -15,4 +15,4 @@ class CustomAdmin(admin.ModelAdmin):
     
 
 
-admin.site.register(CustomUser, CustomAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
