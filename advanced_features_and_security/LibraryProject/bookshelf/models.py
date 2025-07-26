44edf8869ab7738656f2cb1de945976adad8dd51
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-26 12:27:44 +0300

    Refactor CustomUser model to inherit from AbstractUser for improved authentication handling

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Book(models.Model):
@@ -11,7 +11,7 @@ class Book(models.Model):
 date_of_birth: A date field.
 profile_photo: An image field.
 '''        
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     name=models.CharField(max_length=100)
     email=models.EmailField()
     date_of_birth=models.DateField()
