db999a333eba4bdf3829d128ee3f8bc5c982fc17
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-02 22:56:33 +0300

    Refactor URL configuration to include BookViewSet and improve routing

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 
-from django.urls import path
-from views import BookList
+router = DefaultRouter()
+router.register(r'book_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path("books", BookList.as_view(), name="book_list"),
+    path("", include(router.urls)),  
 ]
