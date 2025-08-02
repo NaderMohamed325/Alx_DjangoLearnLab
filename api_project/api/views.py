89e5ec51165a8834a88cc9ebe5dda1cd714bea6c
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-02 22:59:11 +0300

    Remove unused BookList view to streamline viewset implementation

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,10 +6,7 @@ from .serializers import BookSerializer
 from rest_framework import viewsets
 
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    
+
     
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
