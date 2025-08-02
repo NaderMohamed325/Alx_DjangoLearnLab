e16dfa3e8544ce9e8dc62bd5f021b54455c29316
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-02 22:56:40 +0300

    Refactor views to include BookViewSet and ensure proper queryset and serializer usage

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer  
+from rest_framework import viewsets
+
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
