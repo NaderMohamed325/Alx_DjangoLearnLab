0cc1ab8a00254625c9e3b328ae753b16a39e0128
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-04 16:12:57 +0300

    Add authentication classes to REST framework settings in settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'rest_framework.authtoken',
+     'rest_authtoken',
+    'api',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+         'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
