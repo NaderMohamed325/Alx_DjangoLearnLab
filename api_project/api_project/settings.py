8c7cd7ec44b727432740629210038f21db0ea01d
NaderMohamed325 <nadoorclasher@gmail.com>
2025-08-04 16:17:25 +0300

    Refactor REST framework settings to improve authentication and permission class structure

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -46,8 +46,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-         'rest_framework.authentication.TokenAuthentication',
-         'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
